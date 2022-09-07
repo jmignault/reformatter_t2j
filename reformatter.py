5ce8be5767210d29c678293687e6b39b8e6f8485
jmignault <john.mignault@gmail.com>
2022-09-07 17:59:57 -0400

    Pull timestamp calcs  out of interpolation

--- reformatter.py
+++ reformatter.py
@@ -13,7 +13,8 @@ outpath = os.path.join(args.infiles, "processed")
 if not (os.path.exists(outpath)):
    os.mkdir(outpath)
 
-logfn = f"{args.infiles}/{datetime.date.strftime(datetime.date.today(), "%m%d%y")}_processing_log.txt"
+tstamp = datetime.date.strftime(datetime.date.today(), "%m%d%y")
+logfn = f"{args.infiles}/{tstamp}_processing_log.txt"
 
 logf = open(logfn, 'w')
 
@@ -23,9 +24,9 @@ for fn in os.listdir(args.infiles):
     try:
       print(f"Converting {fn}")
       cmdstr = 'soffice --convert-to pdf --outdir "' + outpath + '" "' + args.infiles + '/' + fn + '"'
-      print(cmdstr)
       os.system(cmdstr)
-      logstr = f"{datetime.date.strftime(datetime.date.today(), "%m%d%y:%M:%S")} Converted {fn} to pdf\n"
+      lstamp = datetime.date.strftime(datetime.date.today(), "%m%d%y:%M:%S")
+      logstr = f"{lstamp}: Converted {fn} to pdf\n"
       logf.write(logstr)
     except:
       continue
