b9a4e64729053e7d0e622a2173f40fef0dc3006a
jmignault <john.mignault@gmail.com>
2022-09-07 18:18:01 -0400

    Fix erroneous call to datetime.now()

--- reformatter.py
+++ reformatter.py
@@ -13,7 +13,7 @@ outpath = os.path.join(args.infiles, "processed")
 if not (os.path.exists(outpath)):
    os.mkdir(outpath)
 
-tstamp = datetime.date.strftime(datetime.date.today(), "%m%d%y%M%S")
+tstamp = datetime.date.strftime(datetime.datetime.now(), "%m%d%y%M%S")
 logfn = f"{args.infiles}/{tstamp}_processing_log.txt"
 
 logf = open(logfn, 'w')
@@ -25,7 +25,7 @@ for fn in os.listdir(args.infiles):
       print(f"Converting {fn}")
       cmdstr = 'soffice --convert-to pdf --outdir "' + outpath + '" "' + args.infiles + '/' + fn + '"'
       os.system(cmdstr)
-      lstamp = datetime.date.strftime(datetime.date.today(), "%m%d%y:%M:%S")
+      lstamp = datetime.date.strftime(datetime.datetime.now(), "%m%d%y:%M:%S")
       logstr = f"{lstamp}: Converted {fn} to pdf\n"
       logf.write(logstr)
     except:
