bbcf0cbc0648b442eb2c7683bbd44d976dbeb1dd
jmignault <john.mignault@gmail.com>
2022-10-24 16:29:33 -0400

    Use os.walk to descend into subdirs of argument path

--- reformatter.py
+++ reformatter.py
@@ -11,27 +11,28 @@ parser.add_argument('infiles', help="Folder containing files to be converted")
 
 args = parser.parse_args()
 
-outpath = os.path.join(args.infiles, "processed")
-if not (os.path.exists(outpath)):
-   os.mkdir(outpath)
-
 tstamp = datetime.date.strftime(datetime.datetime.now(), "%m%d%y%M%S")
 logfn = f"{args.infiles}/{tstamp}_processing_log.txt"
 
 logf = open(logfn, 'w')
 
-for fn in os.listdir(args.infiles):
-   ext = os.path.splitext(fn)[-1].lower()
-   if ext in fexts:
-      try:
-         lstamp = datetime.date.strftime(datetime.datetime.now(), "%m-%d-%y:%M:%S")
-         print(f"Converting {fn}")
-         cmdstr = 'soffice --convert-to pdf --outdir "' + outpath + '" "' + args.infiles + '/' + fn + '"'
-         os.system(cmdstr)
-         logstr = f"{lstamp}: Converted {fn} to pdf\n"
-         logf.write(logstr)
-      except BaseException as err:
-         logf.write(f"{lstamp}:Could not convert {fn}: {err}")
-      continue
+for path, subdir, files in os.walk(args.infiles):
+   for name in files:
+      outpath = os.path.join(path, "processed")
+      if not (os.path.exists(outpath)):
+         os.mkdir(outpath)
+      fn = os.path.join(path, name)
+      ext = os.path.splitext(fn)[-1].lower()
+      if ext in fexts:
+         try:
+            lstamp = datetime.date.strftime(datetime.datetime.now(), "%m-%d-%y:%M:%S")
+            print(f"Converting {fn}")
+            cmdstr = 'soffice --convert-to pdf --outdir "' + outpath + '" "' + fn + '"'
+            os.system(cmdstr)
+            logstr = f"{lstamp}: Converted {fn} to pdf\n"
+            logf.write(logstr)
+         except BaseException as err:
+            logf.write(f"{lstamp}:Could not convert {fn}: {err}")
+         continue
 
 logf.close()
