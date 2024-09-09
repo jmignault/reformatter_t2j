ac8c0b06830dbcfd2e8cad8ddd908110188fc9da
jmignault <john.mignault@gmail.com>
2024-09-09 16:00:58 -0400

    Refactor script to convert TIFFs in place to JP2

--- reformatter.py
+++ reformatter.py
@@ -2,38 +2,39 @@
 import os
 import argparse
 import datetime
+from pathlib import Path
 
 # converter functions. Must take 2 arguments: filename and output directory.
 def wp_to_pdf(fn, pdir):
    cmdstr = 'soffice --convert-to pdf --outdir "' + pdir + '" "' + fn + '"'
    os.system(cmdstr)
+
+def tif_to_jpx(fn, pdir):
+   outfn = f'{Path(fn).stem}.jp2'
+   outf = os.path.join(pdir, outfn)
+   print(outf)
+   cmdstr = f'magick {fn} {outf}'
+   os.system(cmdstr)
+
   
 # dictionary of format conversions: keys are extensions, value is function to call for conversion 
-formats = {'.doc':wp_to_pdf, '.docx':wp_to_pdf, '.wp':wp_to_pdf, '.pub':wp_to_pdf, '.txt':wp_to_pdf}
+formats = {'.tif': tif_to_jpx}
 
 # make a list of extensions
 fkeys = list(formats.keys())
 # define arguments and parse them
-parser = argparse.ArgumentParser(description='Convert a folder of files to pdf format.')
+parser = argparse.ArgumentParser(description='Walk a folder, converting TIFF files to JP2 format.')
 parser.add_argument('infiles', help="Folder containing files to be converted")
 
 args = parser.parse_args()
 
-outpath = os.path.join(os.getcwd(), "processed")
-if not (os.path.exists(outpath)):
-   os.mkdir(outpath)
-
 tstamp = datetime.date.strftime(datetime.datetime.now(), "%m%d%y%M%S")
 dname = os.path.basename(os.path.normpath(args.infiles))
-logfn = os.path.join(outpath, f"{tstamp}_{dname}_log.txt")
+logfn = os.path.join(f"{tstamp}_{dname}_log.txt")
 
 logf = open(logfn, 'w', encoding="utf-8")
 
 for path, subdir, files in os.walk(args.infiles):
-
-   procdir = os.path.join(outpath, os.path.relpath(path, os.path.dirname(args.infiles)))
-   if not(os.path.exists(procdir)):
-      os.mkdir(procdir)
 
    for name in files:
       # get a full path to pass to function
@@ -48,8 +49,8 @@ for path, subdir, files in os.walk(args.infiles):
          # yes, print a msg & call function associated with the extension
          try:
             print(f"Converting {fn}")
-            formats[ext](fn, procdir)
-            logstr = f"{lstamp}: Converted {fn} to pdf\n"
+            formats[ext](fn, path)
+            logstr = f"{lstamp}: Converted {fn} to jp2\n"
             logf.write(logstr)
          except BaseException as err:
             logf.write(f"{lstamp}:Could not convert {fn}: {err}")
